616eab594a203f5394c80eb5b466a7172f1a3185
Julian Blasco <blascoblasco9@gmail.com>
2020-05-07 19:35:36 -0300

    Create events controller and create method

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+  include EventsHelper
+  include SessionsHelper
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      flash[:notice] = 'Event created successfuly'
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+end
