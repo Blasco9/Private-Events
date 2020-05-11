f5aa58eca251d6a66df533995ecdf19f9a000a99
Julian Blasco <blascoblasco9@gmail.com>
2020-05-11 19:16:35 -0300

    Add feature to invite users to events

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,17 +1,18 @@
 class EventAttendancesController < ApplicationController
   def create
-    @event_attendance = EventAttendance.new(event_attendance_params)
-    puts params
-    if @event_attendance.valid?
-      puts '------------------------------Success---------------------------------'
+    @event_attendance = EventAttendance.where('event_id = ? AND attendee_id = ?', *attendance_params.values)
+
+    if @event_attendance.exists?
+      redirect_to user_path(current_user), notice: 'User already invited'
     else
-      puts '------------------------------Failure---------------------------------'
+      EventAttendance.create(attendance_params)
+      redirect_to user_path(current_user), notice: 'New attendee invited'
     end
   end
 
   private
 
-  def event_attendance_params
+  def attendance_params
     params.require(:event_attendance).permit(:event_id, :attendee_id)
   end
 end
