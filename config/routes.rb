f88063241c3a89c895285fab336d9fa385b3b8ef
Julian Blasco <blascoblasco9@gmail.com>
2020-05-14 13:01:18 -0300

    Add bootstrap clases to all views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   resources :events, only: [:index, :show, :new, :create]
   resources :users, only: [:new, :create, :show]
+  root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
